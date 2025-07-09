82ee0d4dfe3031cacd0e60efbba65ba0150e0723
Marcos <marcostulli@hotmail.com>
2025-07-09 16:43:34 +0200

    Try Simpler approach

--- src/main.serverless.ts
+++ src/main.serverless.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import express, { Request, Response } from 'express';
@@ -5,18 +8,18 @@ import { AppModule } from './app.module';
 import { SwaggerModule } from '@nestjs/swagger';
 import { config } from './swagger.config';
 import { OpenAPIObject } from '@nestjs/swagger';
-// test
+import * as swaggerUi from 'swagger-ui-express';
+
 export async function createNestApplication() {
   const server = express();
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
   const document: OpenAPIObject = SwaggerModule.createDocument(app, config);
 
-  server.get('/docs-json', (req: Request, res: Response) => {
-    res.json(document);
-  });
+  server.use('/docs', swaggerUi.serve);
+  server.get('/docs', swaggerUi.setup(document));
 
-  server.get('/docs/swagger.json', (req: Request, res: Response) => {
+  server.get('/docs-json', (req: Request, res: Response) => {
     res.json(document);
   });
 
